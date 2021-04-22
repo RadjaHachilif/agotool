c6f3389acd354091ff5b2128ffad17197fb6dc32
RadjaHachilif <er_hachilif@esi.dz>
2021-04-22 10:17:29 +0200

    update fixpermissions.sh

--- app/static/fixpermissions.sh
+++ app/static/fixpermissions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ##### Aquarius for agotool_PMID_autoupdate
-#### vim /home/dblyon/PMID_autoupdate/agotool/app/static/fixpermissions.sh
+#### vim /home/rhachilif/PMID_autoupdate/agotool/app/static/fixpermissions.sh
 ### this is aimed at fixing file permissions for Nginx on Aquarius
-chmod -R 755 /home/dblyon/PMID_autoupdate/agotool/app/static
-chmod 777 /home/dblyon/PMID_autoupdate/agotool/cron_weekly_Aquarius_update_aGOtool_PMID.sh
+chmod -R 755 /home/rhachilif/PMID_autoupdate/agotool/app/static
+chmod 777 /home/rhachilif/PMID_autoupdate/agotool/cron_weekly_Aquarius_update_aGOtool_PMID.sh
 
-# ### cd /home/dblyon/PMID_autoupdate/agotool/.git/hooks
+# ### cd /home/rhachilif/PMID_autoupdate/agotool/.git/hooks
 # ### vim post-checkout and post-merge
 # #!/bin/sh
-# chmod a+x /home/dblyon/PMID_autoupdate/agotool/app/static/fixpermissions.sh
-# /home/dblyon/PMID_autoupdate/agotool/app/static/fixpermissions.sh
+# chmod a+x /home/rhachilif/PMID_autoupdate/agotool/app/static/fixpermissions.sh
+# /home/rhachilif/PMID_autoupdate/agotool/app/static/fixpermissions.sh
